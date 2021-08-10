9fbf231d040fa66f498ce0c4e2bfa785b332b41d
OlexanderMiroshnychenko <50326548+OlexanderMiroshnychenko@users.noreply.github.com>
2021-08-10 16:00:09 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='xla_gpu',
+      version='1.0',
+      description='make tf 1.15 work with xla_gpu',
+      author='test',
+      author_email='test',
+      url='',
+      packages=['tensorflow'],
+     )
